d8cce4f211ace024a3bb61fd52fd56b0c2408a45
naumanalin <naumanalin865@gmail.com>
2025-10-28 15:18:00 +0500

    Setup Express.js Server

---
+++ PERN-TenStack-crud/backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const corssOptions = {
+  origin: '*', // Adjust this to your frontend's origin
+//   optionsSuccessStatus: 200,
+};
+
+// Middleware
+app.use(cors(corssOptions));
+app.use(bodyParser.json());
+
+
+// API/Endpoints
+app.get('/', (req, res) => {
+  res.json({ success:true, cmd:"Ping - Pong", message: 'Welcome to the PERN Stack CRUD API' });
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
